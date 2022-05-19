96ecc88540e36320885f3ab1f137550a407e4c24
vagi09 <m.vageeshjajur@gmail.com>
2022-05-19 15:14:59 +0530

    more test scripts added

--- cypress/integration/tm-businessDB.js
+++ cypress/integration/tm-businessDB.js
@@ -29,7 +29,7 @@ describe('TM Business Dashbaord Test', () => {
   })
 
 
-  it.only('Home Page Test', () => {
+  it('Home Page Test', () => {
     //expect(true).to.equal(true)
 
     cy.get('[data-testid="CalendarIcon"]').click();
@@ -67,27 +67,37 @@ describe('TM Business Dashbaord Test', () => {
     cy.get('.MuiGrid-container > .MuiButtonBase-root').click();
     cy.get('#demo-simple-select').click()
     cy.get('[data-value="Private Limited"]').click()
-    cy.get('#outlined-basic').type("L01631KA2010PTC096844");
+    cy.get('#outlined-basic').type("L01631KA2010PTC096847");
     cy.get('.MuiInputBase-root > .MuiButtonBase-root').click();
     // const filepath = "images/razor pay.png"
     // cy.get('[data-testid="UploadIcon"] > path')
     // .click({force: true})
     // .attachFile(filepath);
+    //**Add company Details */
+    cy.get(':nth-child(3) > :nth-child(1) > :nth-child(1) > .MuiInputBase-root > #outlined-basic').type("Taskmo");
+    cy.get(':nth-child(3) > :nth-child(2) > :nth-child(1) > .MuiInputBase-root > #outlined-basic').type("Taskmo");
+    cy.get(':nth-child(3) > :nth-child(1) > [style="min-width: 100%;"] > .MuiInputBase-root > #outlined-basic').click();
+    cy.get('[data-value="Service Tech"]').click();
 
+    //** Add founder detaiils *//
+    cy.get('.MuiPaper-root > .MuiGrid-container > :nth-child(1) > .MuiFormControl-root > .MuiInputBase-root > #outlined-basic').type("Taskmo");
+    cy.get('.MuiPaper-root > .MuiGrid-container > :nth-child(2) > .MuiFormControl-root > .MuiInputBase-root > #outlined-basic').type("taskmo@taskmo.com");
+    cy.scrollTo('bottom')
 
-
-
-
-
-
-
-
-  })
-
-
-
-
-
-
+    //** Add company address details */
+    cy.log("Adding Company Details")
+    cy.get(':nth-child(7) > .MuiCardContent-root > [style="background-color: white; border-radius: 10px;"] > .MuiGrid-container.MuiGrid-grid-xs-12 > :nth-child(1) > .MuiTextField-root > .MuiInputBase-root').type("qwefjfjdjfvkf");
+    cy.get(':nth-child(7) > .MuiCardContent-root > [style="background-color: white; border-radius: 10px;"] > .MuiGrid-container.MuiGrid-grid-xs-12 > :nth-child(1) > [style="min-width: 100%;"] > .MuiInputBase-root > #outlined-basic')
+      .click();
+    cy.get('[data-value="Dadra and Nagar Haveli"]').click();
+    cy.get(':nth-child(7) > .MuiCardContent-root > [style="background-color: white; border-radius: 10px;"] > .MuiGrid-container.MuiGrid-grid-xs-12 > :nth-child(2) > :nth-child(1) > .MuiInputBase-root > #outlined-basic')
+      .type("Bengaluru");
+    cy.get(':nth-child(7) > .MuiCardContent-root > [style="background-color: white; border-radius: 10px;"] > .MuiGrid-container.MuiGrid-grid-xs-12 > :nth-child(2) > :nth-child(2) > .MuiInputBase-root > #outlined-basic')
+      .type("560078")
+    cy.get(':nth-child(2) > [style="min-width: 100%;"] > .MuiInputBase-root > #outlined-basic').click();
+    cy.get('#menu-country > .MuiPaper-root > .MuiList-root > .MuiButtonBase-root').click();
+    cy.get('.MuiGrid-justify-content-xs-flex-end > [type="submit"]').click();
+    cy.log("Company created successfully")
+})
 
 })
